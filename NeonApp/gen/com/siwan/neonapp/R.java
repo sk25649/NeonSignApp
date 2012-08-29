15b43dfa47df759c26bea07fe1b13d47c1696536
Siwan Kim <alwayswithjoseph@yahoo.com>
2012-08-29 15:17:18 -0500

    3:17PM

--- NeonApp/gen/com/siwan/neonapp/R.java
+++ NeonApp/gen/com/siwan/neonapp/R.java
@@ -19,10 +19,13 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int addText=0x7f080000;
+        public static final int addText=0x7f080002;
+        public static final int display=0x7f080000;
+        public static final int message=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int add_message=0x7f030001;
     }
     public static final class menu {
         public static final int activity_main=0x7f070000;
@@ -31,6 +34,7 @@ public final class R {
         public static final int Add_Text=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int message=0x7f050004;
         public static final int title_activity_main=0x7f050003;
     }
     public static final class style {
--- NeonApp/src/com/siwan/neonapp/MainActivity.java
+++ NeonApp/src/com/siwan/neonapp/MainActivity.java
@@ -4,13 +4,24 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	TextView neon_msg = (TextView) findViewById(R.id.display);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    
+    public String enterMessage(String s){
+    	String final_message = "";
+    	
+    		
+    	return final_message;
     }
 
 /*    @Override
@@ -23,6 +34,11 @@ public class MainActivity extends Activity {
     @Override     
     public boolean onCreateOptionsMenu(Menu menu){
     	new MenuInflater(this).inflate(R.menu.activity_main, menu);
+    	
+    	
+    	setContentView(R.layout.add_message);
+    	
+    	
     	return(super.onCreateOptionsMenu(menu));
     }
 }
