2860906377a9bdb80bb977ee6c15fc5a950f0368
Siwan Kim <alwayswithjoseph@yahoo.com>
2012-08-30 15:43:26 -0500

    adding new files

---
+++ NeonApp/src/com/siwan/neonapp/addMessage.java
@@ -0,0 +1,14 @@
+package com.siwan.neonapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class addMessage extends Activity {
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.message_screen);
+    }
+	
+}
